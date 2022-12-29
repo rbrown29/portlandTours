8c4a52eb13ef15e3a32ce3c74022483681317115
Richard Brown <richardbrown@Richard.local>
2022-12-29 15:22:26 -0800

    adding files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './css/style.css';
+import Home from './components/Home';
+import Header from './components/header';
+import Footer from './components/footer';
+import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom';
+import LandmarksTour from './components/landmarksTour';
+import DowntownTour from './components/downtownTour';
+import Contact from './components/contact';
+import Growth from './components/growthTour';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Header />
+          <nav>
+            <br /><br /><br /><br /><br /><br /><br />
+            <Link to='/'>Home</Link>
+            <Link to='/growthTour'>Growth</Link>
+            <Link to='/landmarksTour'>Landmarks</Link>
+            <Link to='/downtownTour'>Downtown</Link>
+            <Link to='/contact'>Contact</Link>
+          </nav>
+          <Routes>
+            <Route exact path='/' element={<Home />} />
+            <Route path='/growthTour' element={<Growth />} />
+            <Route path='/landmarksTour' element={<LandmarksTour />} />
+            <Route path='/downtownTour' element={<DowntownTour />} />
+            <Route path='/contact' element={<Contact />} />
+          </Routes>
+          <Footer />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
